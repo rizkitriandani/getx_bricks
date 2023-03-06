5cd5641e07a81348daebe735c486c30bd98bc381
rizkitriandani <rizkitriandani@yahoo.co.id>
2023-03-06 11:52:05 +0700

    Added getx_page

---
+++ getx_page/__brick__/{{name.snakeCase()}}/views/{{name.snakeCase()}}_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../presentation_logic/{{name.snakeCase()}}_controller.dart';
+
+class {{name.pascalCase()}}View extends GetView<{{name.pascalCase()}}Controller> {
+  const {{name.pascalCase()}}View({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('{{name.pascalCase()}}View'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          '{{name.pascalCase()}}View is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
