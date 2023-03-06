e8b1fb80df04da134baa01182764fdc9278ba645
rizkitriandani <rizkitriandani@yahoo.co.id>
2023-03-06 11:41:06 +0700

    Checkpoint.

--- getx_feature/hooks/pre_gen.dart
+++ getx_feature/hooks/pre_gen.dart
@@ -1,10 +1,41 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
+import 'package:path/path.dart' as path;
 
 void run(HookContext context) async {
   context.logger.info('Running pre_gen.dart');
-  final brick = Brick.git(GitPath(
-      'https://github.com/rizkitriandani/getx_bricks.git',
-      path: 'getx_feature'));
-  final generator = await MasonGenerator.fromBrick(brick);
-  
+  final name = context.vars['name'] as String;
+
+  final featuresDirectory = Directory(path.join(
+    Directory.current.path,
+    'lib',
+    'features',
+  ));
+
+  if (featuresDirectory.existsSync()) {
+    final brick = Brick.git(GitPath(
+        'https://github.com/rizkitriandani/getx_bricks.git',
+        path: 'getx_feature'));
+    final generator = await MasonGenerator.fromBrick(brick);
+    final target = DirectoryGeneratorTarget(featuresDirectory);
+    var vars = <String, dynamic>{'name': name};
+    await generator.hooks.preGen(vars: vars, onVarsChanged: (v) => vars = v);
+    final files = await generator.generate(target,
+        vars: vars,
+        logger: context.logger,
+        fileConflictResolution: FileConflictResolution.prompt);
+  } else {
+    final brick = Brick.git(GitPath(
+        'https://github.com/rizkitriandani/getx_bricks.git',
+        path: 'getx_feature'));
+    final generator = await MasonGenerator.fromBrick(brick);
+    final target = DirectoryGeneratorTarget(featuresDirectory);
+    var vars = <String, dynamic>{'name': name};
+    await generator.hooks.preGen(vars: vars, onVarsChanged: (v) => vars = v);
+    final files = await generator.generate(target,
+        vars: vars,
+        logger: context.logger,
+        fileConflictResolution: FileConflictResolution.prompt);
+  }
 }
