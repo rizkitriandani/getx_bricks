ee89c82746cc87ea28f6cdc4d97b939147dd0bf2
rizkitriandani <rizkitriandani@yahoo.co.id>
2023-03-06 11:23:00 +0700

    getx_feature works well.

--- getx_feature/hooks/pre_gen.dart
+++ getx_feature/hooks/pre_gen.dart
@@ -1,5 +1,10 @@
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add pre-generation logic.
+void run(HookContext context) async {
+  context.logger.info('Running pre_gen.dart');
+  final brick = Brick.git(GitPath(
+      'https://github.com/rizkitriandani/getx_bricks.git',
+      path: 'getx_feature'));
+  final generator = await MasonGenerator.fromBrick(brick);
+  
 }
