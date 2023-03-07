8c73ffa046f02ee806770416dcd11d93fc6dc818
rizkitriandani <rizkitriandani@yahoo.co.id>
2023-03-07 21:13:19 +0700

    test

--- getx_feature/hooks/pre_gen.dart
+++ getx_feature/hooks/pre_gen.dart
@@ -7,32 +7,32 @@ void run(HookContext context) async {
   context.logger.info('Running pre_gen.dart');
   final name = context.vars['name'] as String;
 
-  context.logger.info('name: $name');
+  // context.logger.info('name: $name');
 
-  final featuresDirectory = Directory(path.join(
-    Directory.current.path,
-    'lib',
-    'features',
-  ));
-  context.logger.info('featuresDirectory: $featuresDirectory');
-    // context.logger.info('folder features already exist');
-  try {
-    final brick = Brick.git(GitPath(
-        'https://github.com/rizkitriandani/getx_bricks.git',
-        path: 'getx_feature'));
-    final generator = await MasonGenerator.fromBrick(brick);
-    final target = DirectoryGeneratorTarget(featuresDirectory);
-    var vars = <String, dynamic>{'name': name};
-    await generator.hooks.preGen(vars: vars, onVarsChanged: (v) => vars = v);
-    final files = await generator.generate(target,
-        vars: vars,
-        logger: context.logger,
-        fileConflictResolution: FileConflictResolution.prompt);
+  // final featuresDirectory = Directory(path.join(
+  //   Directory.current.path,
+  //   'lib',
+  //   'features',
+  // ));
+  // context.logger.info('featuresDirectory: $featuresDirectory');
+  //   // context.logger.info('folder features already exist');
+  // try {
+  //   final brick = Brick.git(GitPath(
+  //       'https://github.com/rizkitriandani/getx_bricks.git',
+  //       path: 'getx_feature'));
+  //   final generator = await MasonGenerator.fromBrick(brick);
+  //   final target = DirectoryGeneratorTarget(featuresDirectory);
+  //   var vars = <String, dynamic>{'name': name};
+  //   await generator.hooks.preGen(vars: vars, onVarsChanged: (v) => vars = v);
+  //   final files = await generator.generate(target,
+  //       vars: vars,
+  //       logger: context.logger,
+  //       fileConflictResolution: FileConflictResolution.prompt);
 
-    context.logger.info('files: $files');
-  } catch (err) {
-    context.logger.err('error: $err');
-  }
+  //   context.logger.info('files: $files');
+  // } catch (err) {
+  //   context.logger.err('error: $err');
+  // }
 
   // if (featuresDirectory.existsSync()) {
   //   context.logger.info('folder features already exist');
